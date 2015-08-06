49f3175db70e2fa72ffe1d99993dc62e7f674c64
gufran <gufran.xpertsinfosoft@gmail.com>
2015-08-06 13:20:42 +0530

    Fisrt Commit

---
+++ app/src/main/java/com/appxperts/annostudy2/MainActivity.java
@@ -0,0 +1,43 @@
+package com.appxperts.annostudy2;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+@EActivity
+public class MainActivity extends AppCompatActivity {
+
+    @ViewById
+    EditText userNameEditText;
+    @ViewById
+    LinearLayout myLinearLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("GUFRAN", "Inside on create ");
+    }
+
+
+    @AfterViews
+    void calledAfterViewInjection() {
+        Log.d("GUFRAN", "calledAfterViewInjection");
+    }
+
+
+    public void clickMe(View v) {
+        userNameEditText.setText("Hello Dosto");
+        myLinearLayout.setBackgroundColor(Color.RED);
+    }
+
+
+}
